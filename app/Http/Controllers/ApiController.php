2b028fc797ff97f8dcfafe316b1abf459231e10c
Inma <lopuga@gmail.com>
2025-02-05 14:07:08 +0100

    Añadir desde API
    Funcionando

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,11 +6,41 @@ use App\Models\Solicitudes;  // Asegúrate de que esta línea esté presente, y 
 use App\Models\Actividades;  // Asegúrate de que esta línea esté presente, y esté apuntando al modelo, no al controlador
 use Illuminate\Http\Request;
 
-class ApiController extends Controller
-{
+class ApiController extends Controller{
+
+    
+    //Función para leer
     public function index()
     {
         $solicitudes = Solicitudes::with('actividad')->get();
         return response()->json($solicitudes);
     }
+
+    // Función para add nueva solicitud a través de API
+    public function add(Request $request){
+       try {
+        request ()->validate([
+            'dni' => 'required|string',
+            'nombre' => 'required|string',
+            'email' => 'required|string',
+            'telefono' => 'required|integer',
+            'actividad_id' => 'required|integer',
+
+        ]);
+        //Rescatar los datos y almacenarlos en la base de datos
+        $solicitudes = new Solicitudes();
+        $solicitudes->dni = $request->input('dni');
+        $solicitudes->nombre = $request->input('nombre');
+        $solicitudes->email = $request->input('email');
+        $solicitudes->telefono = $request->input('telefono');
+        $solicitudes->actividad_id = $request->input('actividad_id');
+        //Con Eloquent se puede guardar los datos en la base de datos
+        $solicitudes->save();
+        //Devolver la respuesta añadida
+        return response()->json($solicitudes, 201);
+
+    } catch (\Exception $e) {
+        return response()->json(['error' => 'Error'], 500);
+    }
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
 Route::middleware('api')->get('/solicitudes', [ApiController::class, 'index']);
+Route::middleware('api')->post('/solicitudes', [ApiController::class, 'add']);
 
